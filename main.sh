842abc6ec433fa5d8945008ff5ded28782db4057
rooler <roel.wier@gmail.com>
2023-09-14 14:10:45 -0400

    uncomment main script

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 #/bin/bash
-# python -u ./scripts/prepare.py
-# sh ./scripts/test.sh
-# sh ./scripts/classify.sh
-# sh ./scripts/associative.sh
-# sh ./scripts/commutants.sh
+ python -u ./scripts/prepare.py
+ sh ./scripts/test.sh
+ sh ./scripts/classify.sh
+ sh ./scripts/associative.sh
+ sh ./scripts/commutants.sh
 
 python -u ./scripts/plot_dimensions.py
